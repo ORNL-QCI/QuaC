e930128004b6e875379e277387a952b4d1164d7a
Thien Nguyen <nguyentm@ornl.gov>
2020-04-02 15:10:02 -0400

    Implement U3 cmd-def for the 2-qubit default backend
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc/Backend/DefaultBackend2Q.hpp
+++ xacc/Backend/DefaultBackend2Q.hpp
@@ -3,6 +3,8 @@
 #include "xacc.hpp"
 #include "xacc_service.hpp"
 #include "PulseSystemModel.hpp"
+#include "PulseGen.hpp"
+#include "Pulse.hpp"
 
 namespace QuaC {
 class Default2Q : public PulseSystemModel
@@ -12,11 +14,13 @@ public:
     Default2Q() 
     {
         // Hamiltonian: full two-qubit transmon Hamiltonian (includes the coupling terms)
+        // We turn-off j (Q-Q coupling) for the time being:
+        // "j": 0.062831853072
         const std::string hamiltonianJson = R"#(
             {
                 "description": "Two-qubit Hamiltonian.",
                 "h_latex": "",
-                "h_str": ["0.5*(2*v0-alpha0)*O0", "0.5*alpha0*O0*O0", "r*X0||D0", "r*X0||U1", "r*X1||U0", "0.5*(2*v1-alpha1)*O1", "0.5*alpha1*O1*O1", "r*X1||D1", "j*(Sp0*Sm1+Sm0*Sp1)"],
+                "h_str": ["0.5*(2*v0-alpha0)*O0", "0.5*alpha0*O0*O0", "r*(SM0 + SP0)||D0", "r*(SM0 + SP0)||U1", "r*(SM1 + SP1)||U0", "0.5*(2*v1-alpha1)*O1", "0.5*alpha1*O1*O1", "r*(SM1 + SP1)||D1", "j*(Sp0*Sm1+Sm0*Sp1)"],
                 "osc": {},
                 "qub": {
                     "0": 2,
@@ -27,8 +31,8 @@ public:
                     "v1": 32.044245066616,
                     "alpha0": -2.073451151369, 
                     "alpha1": -2.073451151369,
-                    "r": 0.125663706144,
-                    "j": 0.062831853072
+                    "r": 0.0314,
+                    "j": 0.0
                 }
             }
         )#";
@@ -36,14 +40,20 @@ public:
 
         BackendChannelConfigs channelConfigs;
         channelConfigs.dt = 1.0;
-        channelConfigs.loFregs_dChannels.emplace_back(4.9);
         channelConfigs.loFregs_dChannels.emplace_back(5.0);
-        channelConfigs.loFregs_uChannels.emplace_back(4.9);
+        channelConfigs.loFregs_dChannels.emplace_back(5.1);
         channelConfigs.loFregs_uChannels.emplace_back(5.0);
+        channelConfigs.loFregs_uChannels.emplace_back(5.1);
         // Pulse library set-up:
-        // TODO:
-        channelConfigs.addOrReplacePulse("pulse1", {});
+        // PI/2 rotation
+        const int PULSE_PI_2_DURATION = 50;
+        channelConfigs.addOrReplacePulse("X0_PI_2", QuaC::SquarePulse(PULSE_PI_2_DURATION));
+        channelConfigs.addOrReplacePulse("X1_PI_2", QuaC::SquarePulse(PULSE_PI_2_DURATION));        
         setChannelConfigs(channelConfigs);
+
+        // We will use the following pulse decomposition:
+        // u3(θ, φ, λ) := U(θ, φ, λ) = Rz(φ + 3π)Rx(π/2)Rz(θ + π)Rx(π/2)Rz(λ)
+        // i.e. two X(pi/2) pulses and a couple of frame changes
 
         // Add command defs:
         // Needs U3_0, U3_1, CX_0_1 and CX_1_0 
@@ -52,6 +62,30 @@ public:
             auto cmddef_u3_0 = provider->createComposite("pulse::u3_0");
             cmddef_u3_0->addVariables({"P0", "P1", "P2"});
             cmddef_u3_0->setBits({0});
+            {
+                auto fc_P0 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+                xacc::InstructionParameter fcParameterP0("P0 + pi");
+                fc_P0->setParameter(0, fcParameterP0);
+                fc_P0->setStart(PULSE_PI_2_DURATION);
+
+                auto fc_P1 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+                xacc::InstructionParameter fcParameterP1("P1 + 3*pi");
+                fc_P1->setParameter(0, fcParameterP1);
+                fc_P1->setStart(PULSE_PI_2_DURATION + PULSE_PI_2_DURATION);
+
+                auto fc_P2 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+                xacc::InstructionParameter fcParameterP2("P2");
+                fc_P2->setParameter(0, fcParameterP2);
+                fc_P2->setStart(0);
+                
+                auto firstPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X0_PI_2", "d0");
+                firstPulseX_Pi_2->setStart(0);
+                
+                auto secondPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X0_PI_2", "d0");
+                secondPulseX_Pi_2->setStart(PULSE_PI_2_DURATION);
+                // Add pulse sequence to the cmd-def
+                cmddef_u3_0->addInstructions({fc_P2, firstPulseX_Pi_2, fc_P0, secondPulseX_Pi_2, fc_P1});
+            }
 
             auto cmddef_u3_1 = provider->createComposite("pulse::u3_1");
             cmddef_u3_1->addVariables({"P0", "P1", "P2"});
--- xacc/tests/DefaultBackendTester.cpp
+++ xacc/tests/DefaultBackendTester.cpp
@@ -1,11 +1,49 @@
 #include <gtest/gtest.h>
 #include "xacc.hpp"
 
-
-TEST(DefaultBackendTester, checkSimple) 
+TEST(DefaultBackendTester, checkSingleQubitGate) 
 {
-    auto qpu = xacc::getAccelerator("QuaC:Default2Q");    
+    const int NB_SHOTS = 10000;
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // X gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test1(qbit q) {
+            X(q[0]);
+            Measure(q[0]);
+        })", qpu);
 
+        auto program = ir->getComposite("test1");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        // Should be in |1> state
+        EXPECT_NEAR(prob0, 0.0, 0.1);
+        EXPECT_NEAR(prob1, 1.0, 0.1);  
+    }
+    
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // H gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test2(qbit q) {
+            H(q[0]);
+            Measure(q[0]);
+        })", qpu);
+
+        auto program = ir->getComposite("test2");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        // Should be in 50-50 state
+        EXPECT_NEAR(prob0, 0.5, 0.1);
+        EXPECT_NEAR(prob1, 0.5, 0.1);  
+    }
+    
 }
 
 int main(int argc, char **argv) 
