90059b0a0fc03d14fd79ef4e113e22b027e6dd06
Thien Nguyen <nguyentm@ornl.gov>
2020-05-06 16:50:28 -0400

    Added single-qubit pulses for the cached default backend
    Next step is to add the CNOT pulse from calibration run.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- xacc/Backend/DefaultBackend2Q.hpp
+++ xacc/Backend/DefaultBackend2Q.hpp
@@ -54,7 +54,6 @@ public:
             // These params are pre-calibrated (nl-opt)
             const double ampl = 0.169244;
             const double beta = 1.0;
-            const double fcAngle = 1.56906;
             const int nSamples = 121;
             const int gamma = 20;
 
@@ -65,107 +64,237 @@ public:
             auto pulse = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
             pulse->setBits({0});
 
-            auto fcPre = std::make_shared<xacc::quantum::Pulse>("fc", channelName);
-            xacc::InstructionParameter fcPreParameter(fcAngle);
-            fcPre->setParameter(0, fcPreParameter);
-            fcPre->setBits({0});
-
-            auto fcPost = std::make_shared<xacc::quantum::Pulse>("fc", channelName);
-            xacc::InstructionParameter fcPostParameter(-fcAngle);
-            fcPost->setParameter(0, fcPostParameter);
-            fcPost->setBits({0});
-            fcPost->setStart(nSamples);
-
-            cmddef_x_0->addInstructions({ fcPre, pulse, fcPost });
+            cmddef_x_0->addInstructions({ pulse });
             xacc::contributeService(cmddef_x_0->name(), cmddef_x_0);
         }
-       
 
-        // ===============================================     
-
-        
-        // PI/2 rotation
-        const int PULSE_PI_2_DURATION = 50;
-        channelConfigs.addOrReplacePulse("X0_PI_2", QuaC::SquarePulse(PULSE_PI_2_DURATION));
-        channelConfigs.addOrReplacePulse("X1_PI_2", QuaC::SquarePulse(PULSE_PI_2_DURATION));        
-        setChannelConfigs(channelConfigs);
-
-        // We will use the following pulse decomposition:
-        // u3(θ, φ, λ) := U(θ, φ, λ) = Rz(φ + 3π)Rx(π/2)Rz(θ + π)Rx(π/2)Rz(λ)
-        // i.e. two X(pi/2) pulses and a couple of frame changes
-
-        // Add command defs:
-        // Needs U3_0, U3_1, CX_0_1 and CX_1_0 
+        // H gate:
         {
-            auto provider = xacc::getIRProvider("quantum");
+            auto cmddef_h_0 = provider->createComposite("pulse::h_0");
+            cmddef_h_0->setBits({0});
+            // These params are pre-calibrated (nl-opt)
+            const double ampl = 0.083113;
+            const double beta = 1.0;
+            const int nSamples = 121;
+            const int gamma = 20;
+
+            const std::string pulseName = "drag_" + std::to_string(ampl) + "_" + std::to_string(beta);
+            channelConfigs.addOrReplacePulse(pulseName, QuaC::Drag(nSamples, ampl, gamma, beta));
+            const std::string channelName = "d0";
+
+            auto pulse1 = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
+            pulse1->setBits({0});
+
+            xacc::InstructionParameter fcParameter(M_PI_2);
+            auto fcInst1 = std::make_shared<xacc::quantum::Pulse>("fc", channelName);
+            fcInst1->setParameter(0, fcParameter);
+            fcInst1->setBits({0});
+            fcInst1->setStart(nSamples);
+
+            auto fcInst2 = std::make_shared<xacc::quantum::Pulse>("fc", "u0");
+            fcInst2->setParameter(0, fcParameter);
+            fcInst2->setBits({0});
+            fcInst2->setStart(nSamples);
+            
+            auto pulse2 = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
+            pulse2->setBits({0});
+            pulse2->setStart(nSamples);
+
+            cmddef_h_0->addInstructions({ pulse1, fcInst1, fcInst2, pulse2 });
+            xacc::contributeService(cmddef_h_0->name(), cmddef_h_0);
+        }
+
+        // U3 gate
+        {
+            // These params are pre-calibrated (nl-opt)
+            const double ampl = 0.083113;
+            const double beta = 1.0;
+            const int nSamples = 121;
+            const int gamma = 20;
+
+            const std::string pulseName = "drag_" + std::to_string(ampl) + "_" + std::to_string(beta);
+            channelConfigs.addOrReplacePulse(pulseName, QuaC::Drag(nSamples, ampl, gamma, beta));
+            
             auto cmddef_u3_0 = provider->createComposite("pulse::u3_0");
             cmddef_u3_0->addVariables({"P0", "P1", "P2"});
             cmddef_u3_0->setBits({0});
-            {
-                auto fc_P0 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
-                xacc::InstructionParameter fcParameterP0("P0 + pi");
-                fc_P0->setParameter(0, fcParameterP0);
-                fc_P0->setStart(PULSE_PI_2_DURATION);
 
-                auto fc_P1 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
-                xacc::InstructionParameter fcParameterP1("P1 + 3*pi");
-                fc_P1->setParameter(0, fcParameterP1);
-                fc_P1->setStart(PULSE_PI_2_DURATION + PULSE_PI_2_DURATION);
+            auto fc_P0_d = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+            xacc::InstructionParameter fcParameterP0("pi - P0");
+            fc_P0_d->setParameter(0, fcParameterP0);
+            fc_P0_d->setStart(nSamples);
+            fc_P0_d->setBits({0});
 
-                auto fc_P2 = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
-                xacc::InstructionParameter fcParameterP2("P2");
-                fc_P2->setParameter(0, fcParameterP2);
-                fc_P2->setStart(0);
-                
-                auto firstPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X0_PI_2", "d0");
-                firstPulseX_Pi_2->setStart(0);
-                
-                auto secondPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X0_PI_2", "d0");
-                secondPulseX_Pi_2->setStart(PULSE_PI_2_DURATION);
-                // Add pulse sequence to the cmd-def
-                cmddef_u3_0->addInstructions({fc_P2, firstPulseX_Pi_2, fc_P0, secondPulseX_Pi_2, fc_P1});
-            }
+            auto fc_P0_u = std::make_shared<xacc::quantum::Pulse>("fc", "u0");
+            fc_P0_u->setParameter(0, fcParameterP0);
+            fc_P0_u->setStart(nSamples);
+            fc_P0_u->setBits({0});
 
+
+            auto fc_P1_d = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+            xacc::InstructionParameter fcParameterP1("P1 - pi/2");
+            fc_P1_d->setParameter(0, fcParameterP1);
+            fc_P1_d->setStart(nSamples + nSamples);
+            fc_P1_d->setBits({0});
+
+            auto fc_P1_u = std::make_shared<xacc::quantum::Pulse>("fc", "u0");
+            fc_P1_u->setParameter(0, fcParameterP1);
+            fc_P1_u->setStart(nSamples + nSamples);
+            fc_P1_u->setBits({0});
+            
+            auto fc_P2_d = std::make_shared<xacc::quantum::Pulse>("fc", "d0");
+            xacc::InstructionParameter fcParameterP2("P2 - pi/2");
+            fc_P2_d->setParameter(0, fcParameterP2);
+            fc_P2_d->setStart(0);
+            fc_P2_d->setBits({0});
+
+            auto fc_P2_u = std::make_shared<xacc::quantum::Pulse>("fc", "u0");
+            fc_P2_u->setParameter(0, fcParameterP2);
+            fc_P2_u->setStart(0);
+            fc_P2_u->setBits({0});
+
+            auto firstPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>(pulseName, "d0");
+            firstPulseX_Pi_2->setStart(0);
+            firstPulseX_Pi_2->setBits({0});
+            
+            auto secondPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>(pulseName, "d0");
+            secondPulseX_Pi_2->setStart(nSamples);
+            secondPulseX_Pi_2->setBits({0});
+
+            // Add pulse sequence to the cmd-def
+            cmddef_u3_0->addInstructions({fc_P2_d, fc_P2_u, firstPulseX_Pi_2, fc_P0_d, fc_P0_u, secondPulseX_Pi_2, fc_P1_d, fc_P1_u });
+            xacc::contributeService(cmddef_u3_0->name(), cmddef_u3_0);
+        }
+        // * Q1:
+        // X gate:
+        {
+            auto cmddef_x_1 = provider->createComposite("pulse::x_1");
+            cmddef_x_1->setBits({1});
+            // These params are pre-calibrated (nl-opt)
+            const double ampl = 0.169244;
+            const double beta = 1.0;
+            const int nSamples = 121;
+            const int gamma = 20;
+
+            const std::string pulseName = "drag_" + std::to_string(ampl) + "_" + std::to_string(beta);
+            channelConfigs.addOrReplacePulse(pulseName, QuaC::Drag(nSamples, ampl, gamma, beta));
+            const std::string channelName = "d1";
+
+            auto pulse = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
+            pulse->setBits({1});
+
+            cmddef_x_1->addInstructions({ pulse });
+            xacc::contributeService(cmddef_x_1->name(), cmddef_x_1);
+        }
+
+        // H gate:
+        {
+            auto cmddef_h_1 = provider->createComposite("pulse::h_1");
+            cmddef_h_1->setBits({1});
+            // These params are pre-calibrated (nl-opt)
+            const double ampl = 0.079270;
+            const double beta = 1.0;
+            const int nSamples = 121;
+            const int gamma = 20;
+
+            const std::string pulseName = "drag_" + std::to_string(ampl) + "_" + std::to_string(beta);
+            channelConfigs.addOrReplacePulse(pulseName, QuaC::Drag(nSamples, ampl, gamma, beta));
+            const std::string channelName = "d1";
+
+            auto pulse1 = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
+            pulse1->setBits({1});
+
+            xacc::InstructionParameter fcParameter(M_PI_2);
+            auto fcInst1 = std::make_shared<xacc::quantum::Pulse>("fc", channelName);
+            fcInst1->setParameter(0, fcParameter);
+            fcInst1->setBits({1});
+            fcInst1->setStart(nSamples);
+
+            auto fcInst2 = std::make_shared<xacc::quantum::Pulse>("fc", "u1");
+            fcInst2->setParameter(0, fcParameter);
+            fcInst2->setBits({1});
+            fcInst2->setStart(nSamples);
+            
+            auto pulse2 = std::make_shared<xacc::quantum::Pulse>(pulseName, channelName);
+            pulse2->setBits({1});
+            pulse2->setStart(nSamples);
+
+            cmddef_h_1->addInstructions({ pulse1, fcInst1, fcInst2, pulse2 });
+            xacc::contributeService(cmddef_h_1->name(), cmddef_h_1);
+        }
+
+        // U3 gate
+        {
+            // These params are pre-calibrated (nl-opt)
+            const double ampl = 0.079270;
+            const double beta = 1.0;
+            const int nSamples = 121;
+            const int gamma = 20;
+
+            const std::string pulseName = "drag_" + std::to_string(ampl) + "_" + std::to_string(beta);
+            channelConfigs.addOrReplacePulse(pulseName, QuaC::Drag(nSamples, ampl, gamma, beta));
+            
             auto cmddef_u3_1 = provider->createComposite("pulse::u3_1");
             cmddef_u3_1->addVariables({"P0", "P1", "P2"});
             cmddef_u3_1->setBits({1});
-            {
-                auto fc_P0 = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
-                xacc::InstructionParameter fcParameterP0("P0 + pi");
-                fc_P0->setParameter(0, fcParameterP0);
-                fc_P0->setStart(PULSE_PI_2_DURATION);
 
-                auto fc_P1 = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
-                xacc::InstructionParameter fcParameterP1("P1 + 3*pi");
-                fc_P1->setParameter(0, fcParameterP1);
-                fc_P1->setStart(PULSE_PI_2_DURATION + PULSE_PI_2_DURATION);
+            auto fc_P0_d = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
+            xacc::InstructionParameter fcParameterP0("pi - P0");
+            fc_P0_d->setParameter(0, fcParameterP0);
+            fc_P0_d->setStart(nSamples);
+            fc_P0_d->setBits({1});
 
-                auto fc_P2 = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
-                xacc::InstructionParameter fcParameterP2("P2");
-                fc_P2->setParameter(0, fcParameterP2);
-                fc_P2->setStart(0);
-                
-                auto firstPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X1_PI_2", "d1");
-                firstPulseX_Pi_2->setStart(0);
-                
-                auto secondPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>("X1_PI_2", "d1");
-                secondPulseX_Pi_2->setStart(PULSE_PI_2_DURATION);
-                // Add pulse sequence to the cmd-def
-                cmddef_u3_1->addInstructions({fc_P2, firstPulseX_Pi_2, fc_P0, secondPulseX_Pi_2, fc_P1});
-            }
+            auto fc_P0_u = std::make_shared<xacc::quantum::Pulse>("fc", "u1");
+            fc_P0_u->setParameter(0, fcParameterP0);
+            fc_P0_u->setStart(nSamples);
+            fc_P0_u->setBits({1});
 
-            auto cmddef_cx_0_1 = provider->createComposite("pulse::cx_0_1");
-            cmddef_cx_0_1->setBits({0, 1});
+
+            auto fc_P1_d = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
+            xacc::InstructionParameter fcParameterP1("P1 - pi/2");
+            fc_P1_d->setParameter(0, fcParameterP1);
+            fc_P1_d->setStart(nSamples + nSamples);
+            fc_P1_d->setBits({1});
+
+            auto fc_P1_u = std::make_shared<xacc::quantum::Pulse>("fc", "u1");
+            fc_P1_u->setParameter(0, fcParameterP1);
+            fc_P1_u->setStart(nSamples + nSamples);
+            fc_P1_u->setBits({1});
             
-            auto cmddef_cx_1_0 = provider->createComposite("pulse::cx_1_0");
-            cmddef_cx_1_0->setBits({1, 0});
+            auto fc_P2_d = std::make_shared<xacc::quantum::Pulse>("fc", "d1");
+            xacc::InstructionParameter fcParameterP2("P2 - pi/2");
+            fc_P2_d->setParameter(0, fcParameterP2);
+            fc_P2_d->setStart(0);
+            fc_P2_d->setBits({1});
 
-            // TODO: define pulse libs and construct pulse sequence for the above 4 gates.  
-            xacc::contributeService(cmddef_u3_0->name(), cmddef_u3_0);
+            auto fc_P2_u = std::make_shared<xacc::quantum::Pulse>("fc", "u1");
+            fc_P2_u->setParameter(0, fcParameterP2);
+            fc_P2_u->setStart(0);
+            fc_P2_u->setBits({1});
+
+            auto firstPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>(pulseName, "d1");
+            firstPulseX_Pi_2->setStart(0);
+            firstPulseX_Pi_2->setBits({1});
+            
+            auto secondPulseX_Pi_2 = std::make_shared<xacc::quantum::Pulse>(pulseName, "d1");
+            secondPulseX_Pi_2->setStart(nSamples);
+            secondPulseX_Pi_2->setBits({1});
+
+            // Add pulse sequence to the cmd-def
+            cmddef_u3_1->addInstructions({fc_P2_d, fc_P2_u, firstPulseX_Pi_2, fc_P0_d, fc_P0_u, secondPulseX_Pi_2, fc_P1_d, fc_P1_u });
             xacc::contributeService(cmddef_u3_1->name(), cmddef_u3_1);
-            xacc::contributeService(cmddef_cx_0_1->name(), cmddef_cx_0_1);
-            xacc::contributeService(cmddef_cx_1_0->name(), cmddef_cx_0_1);
-        } 
+        }
+
+
+        // CNOT gate: 
+        // NOTE: We temporary use digital U3 gates/pseudo pulses to
+        // correct local roration errors pre- and post- CR pulses.
+        // We will replace them  which actual pulses later.
+        // Rationale: This will save significant simulation time during development/testing.
+        // ===============================================     
+
+
+        setChannelConfigs(channelConfigs);
     }
 
     const std::string name() const override { return "Default2Q"; }
--- xacc/tests/DefaultBackendTester.cpp
+++ xacc/tests/DefaultBackendTester.cpp
@@ -1,7 +1,12 @@
 #include <gtest/gtest.h>
 #include "xacc.hpp"
 
-TEST(DefaultBackendTester, checkGateX) 
+namespace {
+    // Tolerance epsilon for numerical errors
+    const double TOL_EPS = 0.1;
+}
+
+TEST(DefaultBackendTester, checkSingleGateQ0) 
 {
     const int NB_SHOTS = 10000;
     {
@@ -21,68 +26,170 @@ TEST(DefaultBackendTester, checkGateX)
         const double prob0 = qubitReg->computeMeasurementProbability("0");
         const double prob1 = qubitReg->computeMeasurementProbability("1");
         // Should be in |1> state
-        EXPECT_NEAR(prob0, 0.0, 0.1);
-        EXPECT_NEAR(prob1, 1.0, 0.1);  
+        EXPECT_NEAR(prob0, 0.0, TOL_EPS);
+        EXPECT_NEAR(prob1, 1.0, TOL_EPS);  
     }
     
-    // {
-    //     auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
     
-    //     auto xasmCompiler = xacc::getCompiler("xasm");
-    //     // H gate:
-    //     auto ir = xasmCompiler->compile(R"(__qpu__ void test2(qbit q) {
-    //         H(q[0]);
-    //         Measure(q[0]);
-    //     })", qpu);
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // H gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test2(qbit q) {
+            H(q[0]);
+            Measure(q[0]);
+        })", qpu);
 
-    //     auto program = ir->getComposite("test2");
-    //     auto qubitReg = xacc::qalloc(2);    
-    //     qpu->execute(qubitReg, program);
-    //     const double prob0 = qubitReg->computeMeasurementProbability("0");
-    //     const double prob1 = qubitReg->computeMeasurementProbability("1");
-    //     // Should be in 50-50 state
-    //     EXPECT_NEAR(prob0, 0.5, 0.1);
-    //     EXPECT_NEAR(prob1, 0.5, 0.1);  
-    // }
-    // {
-    //     auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
-    
-    //     auto xasmCompiler = xacc::getCompiler("xasm");
-    //     // X gate:
-    //     auto ir = xasmCompiler->compile(R"(__qpu__ void test3(qbit q) {
-    //         X(q[1]);
-    //         Measure(q[1]);
-    //     })", qpu);
+        auto program = ir->getComposite("test2");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        qubitReg->print();
 
-    //     auto program = ir->getComposite("test3");
-    //     auto qubitReg = xacc::qalloc(2);    
-    //     qpu->execute(qubitReg, program);
-    //     const double prob0 = qubitReg->computeMeasurementProbability("0");
-    //     const double prob1 = qubitReg->computeMeasurementProbability("1");
-    //     // Should be in |1> state
-    //     EXPECT_NEAR(prob0, 0.0, 0.1);
-    //     EXPECT_NEAR(prob1, 1.0, 0.1);  
-    // }
-    
-    // {
-    //     auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
-    
-    //     auto xasmCompiler = xacc::getCompiler("xasm");
-    //     // H gate:
-    //     auto ir = xasmCompiler->compile(R"(__qpu__ void test4(qbit q) {
-    //         H(q[1]);
-    //         Measure(q[1]);
-    //     })", qpu);
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        // Should be in 50-50 state
+        EXPECT_NEAR(prob0, 0.5, TOL_EPS);
+        EXPECT_NEAR(prob1, 0.5, TOL_EPS);  
+    }
 
-    //     auto program = ir->getComposite("test4");
-    //     auto qubitReg = xacc::qalloc(2);    
-    //     qpu->execute(qubitReg, program);
-    //     const double prob0 = qubitReg->computeMeasurementProbability("0");
-    //     const double prob1 = qubitReg->computeMeasurementProbability("1");
-    //     // Should be in 50-50 state
-    //     EXPECT_NEAR(prob0, 0.5, 0.1);
-    //     EXPECT_NEAR(prob1, 0.5, 0.1);  
-    // }
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+        // Test Hadamard-Hadamard is equivalent to Identity
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // H gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test3(qbit q) {
+            H(q[0]);
+            H(q[0]);
+            Measure(q[0]);
+        })", qpu);
+
+        auto program = ir->getComposite("test3");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        qubitReg->print();
+
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        EXPECT_NEAR(prob0, 1.0, TOL_EPS);
+        EXPECT_NEAR(prob1, 0.0, TOL_EPS);  
+    }
+
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        auto ir = xasmCompiler->compile(R"(__qpu__ void testRx(qbit q, double t) {
+            Rx(q[0], t);
+            Measure(q[0]);
+        })", qpu);
+
+        auto program = ir->getComposite("testRx");
+
+        const auto angles = xacc::linspace(-xacc::constants::pi, xacc::constants::pi, 20);
+        for (const auto& a : angles) 
+        {
+            auto buffer = xacc::qalloc(2);
+            auto evaled = program->operator()({a});
+            qpu->execute(buffer, evaled);
+
+            const double prob0 = buffer->computeMeasurementProbability("0");
+            const double expectedResult = std::cos(a/2.0)*std::cos(a/2.0);
+            std::cout << "Angle = " << a << ": Prob(0) = " << prob0 << " vs. Expected = " << expectedResult << "\n";
+            EXPECT_NEAR(prob0, expectedResult, TOL_EPS);
+        }
+    }
+}
+
+TEST(DefaultBackendTester, checkSingleGateQ1) 
+{
+    const int NB_SHOTS = 10000;
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // X gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test4(qbit q) {
+            X(q[1]);
+            Measure(q[1]);
+        })", qpu);
+
+        auto program = ir->getComposite("test4");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        qubitReg->print();
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        // Should be in |1> state
+        EXPECT_NEAR(prob0, 0.0, TOL_EPS);
+        EXPECT_NEAR(prob1, 1.0, TOL_EPS);  
+    }
+    
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // H gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test5(qbit q) {
+            H(q[1]);
+            Measure(q[1]);
+        })", qpu);
+
+        auto program = ir->getComposite("test5");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        qubitReg->print();
+
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        // Should be in 50-50 state
+        EXPECT_NEAR(prob0, 0.5, TOL_EPS);
+        EXPECT_NEAR(prob1, 0.5, TOL_EPS);  
+    }
+
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+        // Test Hadamard-Hadamard is equivalent to Identity
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        // H gate:
+        auto ir = xasmCompiler->compile(R"(__qpu__ void test6(qbit q) {
+            H(q[1]);
+            H(q[1]);
+            Measure(q[0]);
+        })", qpu);
+
+        auto program = ir->getComposite("test6");
+        auto qubitReg = xacc::qalloc(2);    
+        qpu->execute(qubitReg, program);
+        qubitReg->print();
+
+        const double prob0 = qubitReg->computeMeasurementProbability("0");
+        const double prob1 = qubitReg->computeMeasurementProbability("1");
+        EXPECT_NEAR(prob0, 1.0, TOL_EPS);
+        EXPECT_NEAR(prob1, 0.0, TOL_EPS);  
+    }
+
+    {
+        auto qpu = xacc::getAccelerator("QuaC:Default2Q", {std::make_pair("shots", NB_SHOTS)});    
+        auto xasmCompiler = xacc::getCompiler("xasm");
+        auto ir = xasmCompiler->compile(R"(__qpu__ void testRx1(qbit q, double t) {
+            Rx(q[1], t);
+            Measure(q[1]);
+        })", qpu);
+
+        auto program = ir->getComposite("testRx1");
+
+        const auto angles = xacc::linspace(-xacc::constants::pi, xacc::constants::pi, 20);
+        for (const auto& a : angles) 
+        {
+            auto buffer = xacc::qalloc(2);
+            auto evaled = program->operator()({a});
+            qpu->execute(buffer, evaled);
+
+            const double prob0 = buffer->computeMeasurementProbability("0");
+            const double expectedResult = std::cos(a/2.0)*std::cos(a/2.0);
+            std::cout << "Angle = " << a << ": Prob(0) = " << prob0 << " vs. Expected = " << expectedResult << "\n";
+            EXPECT_NEAR(prob0, expectedResult, TOL_EPS);
+        }
+    }
 }
 
 int main(int argc, char **argv) 
